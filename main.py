5220ad8824ee3a8721ed43cbe76e31e7499e92a1
MaxStefens22 <816467@mystma.org>
2021-10-05 08:54:17 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+#Create a fuction hasL
+def hasL(w):
+    for i in range(0,len(w)):
+        if (w[i]) =="l":
+            return True 
+    else:
+        return False
+
+print (hasL("dog"))
+
